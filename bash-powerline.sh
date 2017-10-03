38366b974756a59d134d7cd0f2a176f3bef69c86
Riobard <me@riobard.com>
2017-10-03 16:51:49 +0800

    Fixed compatbility of fix for #14

--- bash-powerline.sh
+++ bash-powerline.sh
@@ -183,7 +183,7 @@ __powerline() {
         PS1+="$BG_EXIT$FG_BASE3 $PS_SYMBOL $RESET "
     }
 
-    PROMPT_COMMAND=ps1
+    PROMPT_COMMAND="ps1${PROMPT_COMMAND:+; $PROMPT_COMMAND}"
 }
 
 __powerline
